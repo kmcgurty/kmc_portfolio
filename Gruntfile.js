2e9ef0b9a2d112c2ba919b27cf4e6b8c4d6733e6
kmcgurty <kmcgurty1@gmail.com>
2016-01-14 17:47:59 -0500

    fixed up gruntfile, added js uglify

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,30 +3,52 @@ module.exports = function(grunt) {
 		pkg: grunt.file.readJSON('package.json'),
 
 		watch: {
-			scripts: {
-				files: ['routes/*.js'],
-				tasks: ['jshint', 'express:dev'],
+			grunt: {
+				files: ['Gruntfile.js'],
 				options: {
-					livereload: {
-						host: '192.168.1.1'
-					},
-					spawn: false
-				},
-			},
-			css: {
-				files: 'public/stylesheets/*.scss',
-				options: {
-					livereload: {
-						host: '92.168.1.12'
-					}
+					livereload: false
 				}
+			},
+
+			options: {
+				spawn: false,
+				livereload: {
+					host: '192.168.1.12'
+				}
+			},
+
+			scripts: {
+				files: ['src/javascript/*.js'],
+				tasks: ['build', 'express:dev']
+			},
+
+			css: {
+				files: 'public/stylesheets/*.scss'
+			},
+
+			html: {
+				files: 'views/*.hbs',
 			}
 		},
 
 		jshint: {
-			all: ['*.js', 'routes/*.js'],
+			all: ['*.js', 'src/javascript/*.js'],
 			options: {
 				"esnext": true
+			}
+		},
+
+		uglify: {
+			main: {
+				options: {
+					banner: '/* \n' +
+							' Source: https://github.com/kmcgurty/kmc_website \n' +
+							' Build date: <%= grunt.template.today("mm-dd-yyyy") %> \n' +
+							'*/ \n',
+				},
+				files: {
+					'public/javascript/built.js': ['src/javascript/*.js']
+				}
 			}
 		},
 
@@ -36,30 +58,14 @@ module.exports = function(grunt) {
 					script: 'app.js'
 				}
 			}
-		},
-
-		'sftp-deploy': {
-			build: {
-				auth: {
-					host: 'kmcgurty.com',
-					port: 22,
-					authKey: 'key1'
-				},
-				cache: 'sftpCache.json',
-				src: '/home/kmc/Desktop/code/kmc_portfolio/',
-				dest: './server/',
-				exclusions: ['node_modules/*', '.ftppass'],
-				concurrency: 4,
-				progress: true
-			}
 		}
 	});
 
-	//grunt.loadNpmTasks('grunt-contrib-connect');
 	grunt.loadNpmTasks('grunt-contrib-watch');
 	grunt.loadNpmTasks('grunt-contrib-jshint');
 	grunt.loadNpmTasks('grunt-express-server');
-	grunt.loadNpmTasks('grunt-sftp-deploy');
+	grunt.loadNpmTasks('grunt-contrib-uglify');
 
-	grunt.registerTask('default', ['express:dev', 'watch']);
+	grunt.registerTask('default', ['express:dev', 'build', 'watch']);
+	grunt.registerTask('build', ['jshint', 'uglify']);
 };
